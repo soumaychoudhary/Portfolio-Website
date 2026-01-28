6755ddd25bc2224f0dee4e65e3c21aad650cd47e
soumaychoudhary <wultimate646@gmail.com>
2026-01-28 19:16:07 +0530

    Add Navbar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { Navbar } from "../components/Navbar"
 import { ThemeToggle } from "../components/ThemeToggle"
 import {StarBackground} from '@/components/StarBackground'
 export const Home = ()=>{
@@ -11,7 +12,7 @@ export const Home = ()=>{
       <StarBackground />
 
       {/* NavBar */}
-
+      <Navbar />
 
       {/* Main Content */}
 
